5843ee56ca1ea87e2615c7b97f4e07ffb5b96dea
hyun0618 <jhh48654@gmail.com>
2024-04-23 13:09:07 +0900

    access modifier, getter, setter

---
+++ lab06_class/src/com/itwill/modifier03/Person.java
@@ -0,0 +1,24 @@
+package com.itwill.modifier03;
+
+public class Person {
+	private String name; // 읽기 전용 필드
+	private int age; // 읽기/쓰기 필드
+	
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
