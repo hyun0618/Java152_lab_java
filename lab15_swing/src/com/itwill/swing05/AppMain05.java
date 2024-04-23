47c53c91b07146018a83431c51eaa4af593d278c
hyun0618 <jhh48654@gmail.com>
2024-04-23 13:12:09 +0900

    event listener

--- lab15_swing/src/com/itwill/swing05/AppMain05.java
+++ lab15_swing/src/com/itwill/swing05/AppMain05.java
@@ -10,6 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
 
 public class AppMain05 {
 
@@ -20,8 +21,9 @@ public class AppMain05 {
 	private JLabel lblName;
 	private JLabel lblPhone;
 	private JLabel lblEmail;
-	private JTextArea textResult;
 	private JButton btnInput;
+	private JScrollPane scrollPane;
+	private JTextArea textResult;
 
 	/**
 	 * Launch the application.
@@ -60,21 +62,16 @@ public class AppMain05 {
 		lblName.setBounds(45, 40, 80, 30);
 		frame.getContentPane().add(lblName);
 		
-		lblPhone = new JLabel("전화번호");
-		lblPhone.setFont(new Font("D2Coding", Font.PLAIN, 20));
-		lblPhone.setBounds(45, 80, 80, 30);
-		frame.getContentPane().add(lblPhone);
-		
-		lblEmail = new JLabel("이메일");
-		lblEmail.setFont(new Font("D2Coding", Font.PLAIN, 20));
-		lblEmail.setBounds(45, 120, 80, 30);
-		frame.getContentPane().add(lblEmail);
-		
 		textName = new JTextField();
 		textName.setFont(new Font("D2Coding", Font.PLAIN, 20));
 		textName.setBounds(160, 40, 120, 30);
 		frame.getContentPane().add(textName);
 		textName.setColumns(10);
+		
+		lblPhone = new JLabel("전화번호");
+		lblPhone.setFont(new Font("D2Coding", Font.PLAIN, 20));
+		lblPhone.setBounds(45, 80, 80, 30);
+		frame.getContentPane().add(lblPhone);
 		
 		textPhone = new JTextField();
 		textPhone.setFont(new Font("D2Coding", Font.PLAIN, 20));
@@ -82,6 +79,11 @@ public class AppMain05 {
 		textPhone.setBounds(160, 80, 120, 30);
 		frame.getContentPane().add(textPhone);
 		
+		lblEmail = new JLabel("이메일");
+		lblEmail.setFont(new Font("D2Coding", Font.PLAIN, 20));
+		lblEmail.setBounds(45, 120, 80, 30);
+		frame.getContentPane().add(lblEmail);
+			
 		textEmail = new JTextField();
 		textEmail.setFont(new Font("D2Coding", Font.PLAIN, 20));
 		textEmail.setColumns(10);
@@ -89,28 +91,40 @@ public class AppMain05 {
 		frame.getContentPane().add(textEmail);
 		
 		btnInput = new JButton("입력");
-		btnInput.addActionListener(new ActionListener() {
+		btnInput.addActionListener(new ActionListener() { // 익명 내부 클래스 
+			@Override	
 			public void actionPerformed(ActionEvent e) {
-				String name = textName.getText();
-				String phone = textPhone.getText();
-				String email = textEmail.getText();
-				
-				String msg = String.format("%s : %s : %s", name, phone, email);
-				textResult.setText(msg);
+				handleInputButtonClick();
 			}
 		});
 		btnInput.setFont(new Font("D2Coding", Font.PLAIN, 20));
 		btnInput.setBounds(325, 124, 110, 33);
 		frame.getContentPane().add(btnInput);
 		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(45, 179, 390, 159);
+		frame.getContentPane().add(scrollPane);
+		
 		textResult = new JTextArea();
 		textResult.setFont(new Font("D2Coding", Font.PLAIN, 20));
-		textResult.setBounds(44, 180, 391, 146);
-		frame.getContentPane().add(textResult);
-		
-		
-		
-		
+		scrollPane.setViewportView(textResult);
 	}
-
+		
+		private void handleInputButtonClick() {
+			
+			String name = textName.getText();
+			String phone = textPhone.getText();
+			String email = textEmail.getText();
+			
+			String msg = String.format("이름: %s, 전화번호: %s, 이메일: %s\n", name, phone, email);
+			
+			// JTextArea
+//			textResult.setText(msg);
+			textResult.append(msg);
+				
+			// JTextField에 입력된 내용 지우기.
+			textName.setText("");
+			textPhone.setText("");
+			textEmail.setText("");
+	}
 }
