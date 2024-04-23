ac51a05d06fd002172c67bd18382b1e9bfe78d41
hyun0618 <jhh48654@gmail.com>
2024-04-23 15:27:43 +0900

    variable & operators

---
+++ lab02_variable/src/com/itwill/variable02/VariableMain02.java
@@ -0,0 +1,18 @@
+package com.itwill.variable02;
+
+public class VariableMain02 {
+
+	public static void main(String[] args) {
+		// 정수를 저장하는 변수 x, y를 선언하고, 값 초기화.
+		int x = 100; 
+		int y = 3; 
+
+		// 사칙연산
+		
+		// 나누기 연산자
+		// 1) 정수/정수 = 몫
+		// 2) 실수/실수, 실수/정수, 정수/실수 => 소수점까지 계산
+		
+	}
+
+}
